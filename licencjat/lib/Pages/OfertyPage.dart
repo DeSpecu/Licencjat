283a7c5392f3a8dfd7913505e21412ad7a9cb518
Szymon Boniszewski <szymkop2001@gmail.com>
2024-05-12 12:48:05 +0200

    Updated to Chart

--- licencjat/lib/Pages/OfertyPage.dart
+++ licencjat/lib/Pages/OfertyPage.dart
@@ -4,23 +4,23 @@ import 'package:licencjat/src/theme.dart';
 import 'package:provider/provider.dart';
 import 'package:html/dom.dart' as dom;
 import 'package:http/http.dart' as http;
+import 'package:pie_chart/pie_chart.dart';
 
 
 class OfertyPage extends StatelessWidget {
   OfertyPage({super.key});
 
-
   Future<List<String>> data() async{
-    final url = Uri.parse("https://it.pracuj.pl/praca");
+    final url = Uri.parse("https://it.pracuj.pl/praca/warszawa;wp?rd=100&et=17&itth=75%2C212%2C77%2C54%2C41%2C39%2C35%2C38%2C33%2C43%2C209%2C40%2C37%2C76%2C126%2C36%2C84");
     final wynik = await http.get(url);
     dom.Document html = dom.Document.html(wynik.body);
 
-    final technologie = html.querySelectorAll("div.c13gi8t > div.e1v45ci3 > div.ci1jlvv > div > span").map((e) => e.innerHtml.trim()).toList();
+    final technologie = html.querySelectorAll(".tiles_bo3lbgx > span").map((e) => e.innerHtml.trim()).toList();
+
     return technologie;
   }
 
   late Future<List<String>> futureData = data();
-
   @override
   Widget build(BuildContext context) {
     return Consumer<ModelTheme>(builder: (context, ModelTheme themeNotifier, child){
@@ -39,13 +39,37 @@ class OfertyPage extends StatelessWidget {
           future: futureData,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    return Text(
-                          snapshot.data![index],
-                    );
-                  });
+              return PieChart(
+                dataMap: {"tak":0},
+                animationDuration: Duration(milliseconds: 800),
+                chartLegendSpacing: 32,
+                chartRadius: MediaQuery.of(context).size.width / 3.2,
+                colorList: <Color>[
+                  Colors.greenAccent,
+                ],
+                initialAngleInDegree: 0,
+                chartType: ChartType.ring,
+                ringStrokeWidth: 32,
+                centerText: "HYBRID",
+                legendOptions: const LegendOptions(
+                  showLegendsInRow: false,
+                  legendPosition: LegendPosition.right,
+                  showLegends: true,
+                  legendShape: BoxShape.circle,
+                  legendTextStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                chartValuesOptions: const ChartValuesOptions(
+                  showChartValueBackground: true,
+                  showChartValues: true,
+                  showChartValuesInPercentage: false,
+                  showChartValuesOutside: false,
+                  decimalPlaces: 1,
+                ),
+                // gradientList: ---To add gradient colors---
+                // emptyColorGradient: ---Empty Color gradient---
+              );
             } else if (snapshot.hasError) {
               return Text('${snapshot.error}');
             }
