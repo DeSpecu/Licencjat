cde9dd1b40b950ccdb34deaae852c08515c7102b
Szymon Boniszewski <szymkop2001@gmail.com>
2024-05-14 00:23:51 +0200

    Pie Chart completed

--- licencjat/lib/Pages/OfertyPage.dart
+++ licencjat/lib/Pages/OfertyPage.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:licencjat/src/theme.dart';
@@ -10,77 +11,67 @@ import 'package:pie_chart/pie_chart.dart';
 class OfertyPage extends StatelessWidget {
   OfertyPage({super.key});
 
-  Future<List<String>> data() async{
+  Future<Map<String, double>> data() async{
     final url = Uri.parse("https://it.pracuj.pl/praca/warszawa;wp?rd=100&et=17&itth=75%2C212%2C77%2C54%2C41%2C39%2C35%2C38%2C33%2C43%2C209%2C40%2C37%2C76%2C126%2C36%2C84");
     final wynik = await http.get(url);
     dom.Document html = dom.Document.html(wynik.body);
 
     final technologie = html.querySelectorAll(".tiles_bo3lbgx > span").map((e) => e.innerHtml.trim()).toList();
-
-    return technologie;
+    Map<String, double> mapa = {};
+    List<String> s = [];
+    List<String> filtr = [".NET", "Android", "Angular", "C", "C++", "C#", "CSS", "Java",
+    "JavaScript", "Kotlin", "Linux", "PHP", "Python", "React.js",
+    "React Native", "SQL", "Vue.js"];
+    for(var x in technologie) {
+      if(filtr.contains(x)) {
+        s.add(x);
+      }
+    }
+    log(s.length.toString());
+    for(var x in s){
+      if(mapa[x]==null){
+        mapa[x]=0;
+      }
+      else {
+        mapa[x] = s.where((element) => element == x).length.toDouble();
+      }
+    }
+    return mapa;
   }
 
-  late Future<List<String>> futureData = data();
+  late Future<Map<String, double>> futureData = data();
   @override
   Widget build(BuildContext context) {
     return Consumer<ModelTheme>(builder: (context, ModelTheme themeNotifier, child){
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Oferty"),
-          automaticallyImplyLeading: false,
-          actions: [IconButton(onPressed: () {
-            themeNotifier.isDark ? themeNotifier.isDark = false : themeNotifier.isDark = true;
-            },
-              icon: Icon(themeNotifier.isDark ? Icons.sunny : Icons.dark_mode))
-          ],
-        ),
-      body: Center(
-        child: FutureBuilder<List<String>>(
-          future: futureData,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return PieChart(
-                dataMap: {"tak":0},
-                animationDuration: Duration(milliseconds: 800),
-                chartLegendSpacing: 32,
-                chartRadius: MediaQuery.of(context).size.width / 3.2,
-                colorList: <Color>[
-                  Colors.greenAccent,
-                ],
-                initialAngleInDegree: 0,
-                chartType: ChartType.ring,
-                ringStrokeWidth: 32,
-                centerText: "HYBRID",
-                legendOptions: const LegendOptions(
-                  showLegendsInRow: false,
-                  legendPosition: LegendPosition.right,
-                  showLegends: true,
-                  legendShape: BoxShape.circle,
-                  legendTextStyle: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                chartValuesOptions: const ChartValuesOptions(
-                  showChartValueBackground: true,
-                  showChartValues: true,
-                  showChartValuesInPercentage: false,
-                  showChartValuesOutside: false,
-                  decimalPlaces: 1,
-                ),
-                // gradientList: ---To add gradient colors---
-                // emptyColorGradient: ---Empty Color gradient---
-              );
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
+      return Scaffold(
+          appBar: AppBar(
+            title: const Text("Oferty"),
+            automaticallyImplyLeading: false,
+            actions: [IconButton(onPressed: () {
+              themeNotifier.isDark ? themeNotifier.isDark = false : themeNotifier.isDark = true;
+              },
+                icon: Icon(themeNotifier.isDark ? Icons.sunny : Icons.dark_mode))
+            ],
+          ),
+        body: Center(
+          child: FutureBuilder<Map<String, double>>(
+            future: futureData,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return PieChart(
+                  dataMap: snapshot.data!,
+                  chartValuesOptions: ChartValuesOptions(showChartValuesInPercentage: true),
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
 
-            // By default, show a loading spinner.
-            return const CircularProgressIndicator();
-          },
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
         ),
-      ),
-        );
+      );
   });
   }
-
 }
